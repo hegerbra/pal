9adafa9926d80263d1aa21108f2a856da9c91287
hegerba <hegerba@fit.cvut.cz>
2016-01-16 18:23:43 +0100

    no message

--- ExamBrano/src/pal/Main.java
+++ ExamBrano/src/pal/Main.java
@@ -3,12 +3,6 @@ package pal;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        // asdfasdxf
-        //pato ?
-        // si tu ?
-        //fdgs
-        // zmena saddsafd
-        //gghgh cf
+
     }
 }
